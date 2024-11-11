c12ac3734b005ca119d21a929d46332f87827326
Marcel Joshua <marceljoshua69@gmail.com>
2024-11-12 04:21:35 +0700

    add constant values

---
+++ internal/property/constants.go
@@ -0,0 +1,22 @@
+package property
+
+const (
+	// property location
+	standardValuePerM2 = 10_000_000
+	premiumValuePerM2  = 15_000_000
+
+	// appreciation and depreciation rates
+	appreciationRate        = .05
+	residentialDepreciation = .025
+	commercialDepreciation  = .035
+
+	// location bonus rates
+	premiumBonus = .2
+	cornerBonus  = .15
+
+	// maintenance fees
+	residentialBaseFee = 2_500
+	commercialBaseFee  = 3_500
+	securityFee        = 1_000
+	cleaningFee        = 800
+)
