5026a7eadaa4cc2f9b0ab64e40eee943289fc55c
Marcel Joshua <marceljoshua69@gmail.com>
2024-11-12 17:41:58 +0700

    add program flow to main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/marceljsh/thrivepropcalc/internal/property"
+)
+
 func main() {
-	println("its running")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var records []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		records = append(records, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("error reading input: %v", err)
+		os.Exit(1)
+	}
+
+	if err := property.ProcessRecords(records); err != nil {
+		fmt.Printf("error processing records: %v", err)
+		os.Exit(1)
+	}
 }
