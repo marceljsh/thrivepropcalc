d2cdebf94c08fa045ed2a02c3f00850d86873e25
Marcel Joshua <marceljoshua69@gmail.com>
2024-11-12 17:52:18 +0700

    refactor -> move validateInput function to usecase

--- internal/property/usecase.go
+++ internal/property/usecase.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/marceljsh/thrivepropcalc/pkg/formatter"
@@ -51,6 +52,52 @@ func calculateProperties(properties []Property) (float64, float64) {
 	})
 
 	return totalValue, totalMaintenance
+}
+
+func validateInput(line string, lastTime *time.Time) error {
+	fields := strings.Fields(line)
+	if len(fields) != 8 {
+		return errors.New("input does not follow the correct format")
+	}
+
+	currentTime, err := time.Parse(timestampLayout, fields[0])
+	if err != nil {
+		return errors.New("invalid timestamp format")
+	}
+
+	if !lastTime.IsZero() && currentTime.Sub(*lastTime) > 5*time.Minute {
+		return errors.New("gap between records exceeds 5 minutes")
+	}
+	*lastTime = currentTime
+
+	if fields[1] != "RESIDENTIAL" && fields[1] != "COMMERCIAL" {
+		return errors.New("invalid property type")
+	}
+
+	var area float64
+	if _, err := fmt.Sscan(fields[2], &area); area <= 0 || err != nil {
+		return errors.New("area less than or equal to zero")
+	}
+
+	var year int
+	if _, err := fmt.Sscan(fields[3], &year); year < 1900 || year > time.Now().Year() || err != nil {
+		return errors.New("invalid build year")
+	}
+
+	if fields[4] != "STANDARD" && fields[4] != "PREMIUM" {
+		return errors.New("invalid location type")
+	}
+
+	if fields[5] != "YES" && fields[5] != "NO" && fields[5] != "CORNER" {
+		return errors.New("invalid corner specification")
+	}
+
+	var parking int
+	if _, err := fmt.Sscan(fields[6], &parking); parking < 0 || err != nil {
+		return errors.New("parking units must be non-negative")
+	}
+
+	return nil
 }
 
 func displayResults(totalValue float64, maintenance float64, properties []Property) {
